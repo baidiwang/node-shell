85a1a4cb81deb4b9d893f9f33c12925735b1757d
Baidi Wang <baidiwang30@gmail.com>
2022-05-09 16:45:20 -0400

    done with the basic requirements

---
+++ bash.js
@@ -0,0 +1,13 @@
+//Output a prompt
+process.stdout.write('prompt > ');
+
+// The stdin 'data' event fires after a user types in a line
+process.stdin.on('data', (data) => {
+  const cmd = data.toString().trim(); //remove the newline
+
+  // process.stdout.write('You typed: ' + cmd);
+  if(cmd === 'pwd'){
+    process.stdout.write(__dirname);
+  }
+  process.stdout.write('\nprompt > ');
+});
